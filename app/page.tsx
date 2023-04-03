641b4fc019b81283636253b14cdd804624adb6bd
erdemgonul <ahmeterdemgonul@gmail.com>
2023-04-03 17:54:48 +0300

    add meta description

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,12 @@ import {
   PortfolioSection,
 } from "@/components/HomeSections";
 
+export const metadata = {
+  title: "Erdem Gönül | Frontend Developer",
+  description:
+    "a passionate frontend developer who loves to imagine, design and code. Explore my work and discover my skills in creating beautiful and functional websites that engage and delight users. ahmet erdem gönül.",
+};
+
 export default async function Home() {
   return (
     <div className="homepage">
